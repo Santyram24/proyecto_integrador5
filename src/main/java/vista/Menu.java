ee66010534c905b9542044fe8758d558cf8aa88b
ximen <ximen@DESKTOP-VHB1C5S>
2022-05-31 11:37:30 -0500

    falta ejecutable x2

--- src/main/java/vista/Menu.java
+++ src/main/java/vista/Menu.java
@@ -2,9 +2,9 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
-package com.mycompany.vista;
+package vista;
 
-import static com.mycompany.vista.VentanaPrincipal.tipoEmpleado;
+import static vista.VentanaPrincipal.tipoEmpleado;
 
 /**
  *
--- src/main/java/vista/MostrarDatosrecepcionista.java
+++ src/main/java/vista/MostrarDatosrecepcionista.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
-package com.mycompany.vista;
+package vista;
 
 import controlador.Conexion;
 import java.sql.Connection;
@@ -10,27 +10,26 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import javax.swing.JOptionPane;
 import javax.swing.table.DefaultTableModel;
 
 /**
  *
  * @author Srami
  */
-public class MostrarDatosMozo extends javax.swing.JFrame {
+public class MostrarDatosrecepcionista extends javax.swing.JFrame {
 
     /**
-     * Creates new form MostrarDatosMozo
+     * Creates new form VentanaEliminarDatos
      */
-    DefaultTableModel diseño=new DefaultTableModel();
+    DefaultTableModel diseño = new DefaultTableModel();
     Conexion cc = new Conexion();
-    
-    
-    
+
     public void Listar() {
         LimpiaTabla();
         Connection con = null;
         PreparedStatement pst = null;
-        Statement s =null; 
+        Statement s = null;
         ResultSet rs = null;
         String sql = "SELECT * FROM equipaje";
         try {
@@ -41,9 +40,11 @@ public class MostrarDatosMozo extends javax.swing.JFrame {
             diseño = (DefaultTableModel) tabla.getModel();
             while (rs.next()) {
 
-                equipaje[0] = rs.getString("vuelo");
-                equipaje[1] = rs.getString("idequipaje");
-                equipaje[2] = rs.getString("peso");
+                equipaje[0] = rs.getString("idpasajero");
+                equipaje[1] = rs.getString("nombre");
+                equipaje[2] = rs.getString("vuelo");
+                equipaje[3] = rs.getString("idequipaje");
+                equipaje[4] = rs.getString("peso");
 
                 diseño.addRow(equipaje);
             }
@@ -57,14 +58,35 @@ public class MostrarDatosMozo extends javax.swing.JFrame {
             diseño.removeRow(i);
         }
     }
-    public MostrarDatosMozo() {
+
+    public MostrarDatosrecepcionista() {
         initComponents();
-        String[] titulo= new String[]{"VUELO", "IDEQUIPAJE", "PESO"};
+        String[] titulo = new String[]{"IDPASAJERO", "NOMBRE", "VUELO", "IDEQUIPAJE", "PESO"};
         diseño.setColumnIdentifiers(titulo);
         tabla.setModel(diseño);
         setLocationRelativeTo(null);
         this.setResizable(false);
         Listar();
+    }
+
+    public void eliminar() {
+        Connection con = null;
+        PreparedStatement eliminar = null;                
+        con = cc.realizarConexion();
+        
+        int fila = tabla.getSelectedRow();
+        String valor=tabla.getValueAt(fila, 0).toString();
+        
+        try {            
+            eliminar=con.prepareStatement("DELETE FROM equipaje WHERE idpasajero='"+valor+"'");
+            eliminar.executeUpdate();
+            Listar();
+             JOptionPane.showMessageDialog(this, "Datos Eliminados Correctamente");
+        } catch (Exception e) {
+             JOptionPane.showMessageDialog(this, "Ocurrio un error,comunicate con los desarrolladores ","error",JOptionPane.WARNING_MESSAGE);
+        }     
+        
+
     }
 
     /**
@@ -76,18 +98,14 @@ public class MostrarDatosMozo extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jLabel1 = new javax.swing.JLabel();
-        jPanel1 = new javax.swing.JPanel();
         jScrollPane1 = new javax.swing.JScrollPane();
         tabla = new javax.swing.JTable();
+        botonEliminar = new javax.swing.JButton();
         botonVolver = new javax.swing.JButton();
         jLabel2 = new javax.swing.JLabel();
 
-        jLabel1.setText("jLabel1");
-
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
-
-        jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+        getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
         tabla.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
@@ -102,7 +120,16 @@ public class MostrarDatosMozo extends javax.swing.JFrame {
         ));
         jScrollPane1.setViewportView(tabla);
 
-        jPanel1.add(jScrollPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(70, 170, 470, 270));
+        getContentPane().add(jScrollPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(60, 120, 480, 320));
+
+        botonEliminar.setFont(new java.awt.Font("Arial", 0, 18)); // NOI18N
+        botonEliminar.setText("Eliminar");
+        botonEliminar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                botonEliminarActionPerformed(evt);
+            }
+        });
+        getContentPane().add(botonEliminar, new org.netbeans.lib.awtextra.AbsoluteConstraints(480, 30, -1, -1));
 
         botonVolver.setFont(new java.awt.Font("Arial", 0, 18)); // NOI18N
         botonVolver.setText("Volver");
@@ -111,41 +138,35 @@ public class MostrarDatosMozo extends javax.swing.JFrame {
                 botonVolverActionPerformed(evt);
             }
         });
-        jPanel1.add(botonVolver, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 540, -1, -1));
+        getContentPane().add(botonVolver, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 520, -1, -1));
 
         jLabel2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/wallpaper.jpg"))); // NOI18N
-        jPanel1.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, -1, -1));
-
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-        );
+        jLabel2.setText("jLabel2");
+        jLabel2.setMaximumSize(new java.awt.Dimension(600, 600));
+        getContentPane().add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 600, 570));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
     private void botonVolverActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_botonVolverActionPerformed
         dispose();
-        Menu v= new Menu();
+        Menu v = new Menu();
         v.setVisible(true);
     }//GEN-LAST:event_botonVolverActionPerformed
+
+    private void botonEliminarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_botonEliminarActionPerformed
+        // TODO add your handling code here:
+        eliminar();
+    }//GEN-LAST:event_botonEliminarActionPerformed
 
     /**
      * @param args the command line arguments
      */
-    
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton botonEliminar;
     private javax.swing.JButton botonVolver;
-    private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
-    private javax.swing.JPanel jPanel1;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTable tabla;
     // End of variables declaration//GEN-END:variables
